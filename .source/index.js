717ba8e8ce92e15439d4276faad6551a5ca51358
taniasilvana <taniasilvana.lapalelo@wingscorp.com>
2024-12-13 10:41:39 +0700

    feat(navigation): add nav order and more mdx content

--- .source/index.js
+++ .source/index.js
@@ -1,5 +1,8 @@
 import { toRuntime, toRuntimeAsync } from "fumadocs-mdx"
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=c3c1b973ebadc80eb38ada7bd32c9d772b80fa4e282baa3317bfb07d11382fcd"
+import * as docs_0 from "../content/docs/home.mdx?collection=docs&hash=c3c1b973ebadc80eb38ada7bd32c9d772b80fa4e282baa3317bfb07d11382fcd"
 import * as docs_1 from "../content/docs/project.mdx?collection=docs&hash=c3c1b973ebadc80eb38ada7bd32c9d772b80fa4e282baa3317bfb07d11382fcd"
-export const docs = [toRuntime("doc", docs_0, {"path":"index.mdx","absolutePath":"D:/Data/learn/Next js/practice_mantine/content/docs/index.mdx"}), toRuntime("doc", docs_1, {"path":"project.mdx","absolutePath":"D:/Data/learn/Next js/practice_mantine/content/docs/project.mdx"})];
-export const meta = [];
+import * as docs_2 from "../content/docs/welcome.mdx?collection=docs&hash=c3c1b973ebadc80eb38ada7bd32c9d772b80fa4e282baa3317bfb07d11382fcd"
+import * as docs_3 from "../content/docs/contact/contact-detail.mdx?collection=docs&hash=c3c1b973ebadc80eb38ada7bd32c9d772b80fa4e282baa3317bfb07d11382fcd"
+import * as meta_0 from "../content/docs/meta.json?collection=meta&hash=c3c1b973ebadc80eb38ada7bd32c9d772b80fa4e282baa3317bfb07d11382fcd"
+export const docs = [toRuntime("doc", docs_0, {"path":"home.mdx","absolutePath":"D:/Data/learn/Next js/practice_mantine/content/docs/home.mdx"}), toRuntime("doc", docs_1, {"path":"project.mdx","absolutePath":"D:/Data/learn/Next js/practice_mantine/content/docs/project.mdx"}), toRuntime("doc", docs_2, {"path":"welcome.mdx","absolutePath":"D:/Data/learn/Next js/practice_mantine/content/docs/welcome.mdx"}), toRuntime("doc", docs_3, {"path":"contact\\contact-detail.mdx","absolutePath":"D:/Data/learn/Next js/practice_mantine/content/docs/contact/contact-detail.mdx"})];
+export const meta = [toRuntime("meta", meta_0, {"path":"meta.json","absolutePath":"D:/Data/learn/Next js/practice_mantine/content/docs/meta.json"})];
