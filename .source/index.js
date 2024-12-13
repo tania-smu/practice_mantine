19027e95e3125e586ca3c6c8fd2d7b6c30d80e37
taniasilvana <taniasilvana.lapalelo@wingscorp.com>
2024-12-13 10:53:25 +0700

    feat(quiz): add more for quiz

--- .source/index.js
+++ .source/index.js
@@ -1,4 +1,7 @@
 import { toRuntime, toRuntimeAsync } from "fumadocs-mdx"
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=c3c1b973ebadc80eb38ada7bd32c9d772b80fa4e282baa3317bfb07d11382fcd"
-export const docs = [toRuntime("doc", docs_0, {"path":"index.mdx","absolutePath":"D:/Data/learn/Next js/practice_mantine/content/docs/index.mdx"})];
+import * as docs_0 from "../content/docs/contact-detail.mdx?collection=docs&hash=c3c1b973ebadc80eb38ada7bd32c9d772b80fa4e282baa3317bfb07d11382fcd"
+import * as docs_1 from "../content/docs/home.mdx?collection=docs&hash=c3c1b973ebadc80eb38ada7bd32c9d772b80fa4e282baa3317bfb07d11382fcd"
+import * as docs_2 from "../content/docs/project.mdx?collection=docs&hash=c3c1b973ebadc80eb38ada7bd32c9d772b80fa4e282baa3317bfb07d11382fcd"
+import * as docs_3 from "../content/docs/welcome.mdx?collection=docs&hash=c3c1b973ebadc80eb38ada7bd32c9d772b80fa4e282baa3317bfb07d11382fcd"
+export const docs = [toRuntime("doc", docs_0, {"path":"contact-detail.mdx","absolutePath":"D:/Data/learn/Next js/practice_mantine/content/docs/contact-detail.mdx"}), toRuntime("doc", docs_1, {"path":"home.mdx","absolutePath":"D:/Data/learn/Next js/practice_mantine/content/docs/home.mdx"}), toRuntime("doc", docs_2, {"path":"project.mdx","absolutePath":"D:/Data/learn/Next js/practice_mantine/content/docs/project.mdx"}), toRuntime("doc", docs_3, {"path":"welcome.mdx","absolutePath":"D:/Data/learn/Next js/practice_mantine/content/docs/welcome.mdx"})];
 export const meta = [];
